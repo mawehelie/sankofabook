44665c968a5d08f46d24adf784570584a15e933a
M A Wehelie <mawehelie@gmail.com>
2014-08-17 00:08:34 -0700

    creation of rails application with status update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sankofabook::Application.config.secret_key_base = 'edce5dcfd81468d1821f35730238968cfc89b5d60239c563e08187c83a0ba83d7716aa642b7b36d0345c68e52408369f8529bf45d5495f2d36e888ded6e87fe4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sankofabook::Application.config.session_store :cookie_store, key: '_sankofabook_session'
